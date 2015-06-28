5b0668b55a54cfcb78337eda5387221f4a588813
SilverWingedSeraph <evessarudem@gmail.com>
2015-06-28 09:38:32 -0700

    Added TTL graph and extrema detection on RTT.

--- pyplotping.py
+++ pyplotping.py
@@ -12,6 +12,78 @@ ADDR_REGEX = "([0-9]{1,3}\.){3}([0-9]{1,3})"
 DATA_REGEX = "([0-9]{1,3} byte(s)?)"
 RTT_REGEX = "(time=[0-9][0-9]\.[0-9])"
 TTL_REGEX = "(ttl=[0-9]{1,})"
+ 
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    from numpy import NaN, Inf, arange, isscalar, asarray, array
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = arange(len(v))
+    
+    v = asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = Inf, -Inf
+    mnpos, mxpos = NaN, NaN
+    
+    lookformax = True
+    
+    for i in arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+ 
+    return array(maxtab), array(mintab)
 
 def build_graph_for_ping_file(filepath):
   """
@@ -35,13 +107,44 @@ def build_graph_for_ping_file(filepath):
       rttblob = re.search(RTT_REGEX, line).group()
       rtt = float(rttblob.split('=')[1])
       rtt_list.append(rtt)
+      
+      ttlblob = re.search(TTL_REGEX, line).group()
+      ttl = float(ttlblob.split('=')[1])
+      ttl_list.append(ttl)
   
-  plt.title("Ping times to " + host + " (" + hostaddr + ")")
-  #plt.ylim(0, 120) #Set Y limits    
+  #Generate our graphs - Round Trip Time first
+  plt.figure(1)
+  plt.subplot(2,1,1) #Two rows, one column, fig. 1
+  plt.title("Ping times to " + host + " (" + hostaddr + "), " + str(index) + " samples.")
+  #plt.ylim(0, 120) #Set Y limits
+  plt.xlim(0,index)    
   plt.ylabel("Round Trip Time (ms)")
   plt.xlabel("ICMP Sequence Number")
   plt.grid(True)
   plt.plot(np.array([n for n in range (index)]), np.array(rtt_list), linestyle = '-', marker = '', color = 'r')
+  #Now, find minima and maxima
+  rtt_maxima, rtt_minima = peakdet(np.array(rtt_list), 1)
+  rtt_maxima_xpos = []
+  rtt_maxima_ypos = []
+  rtt_minima_xpos = []
+  rtt_minima_ypos = []
+  for (x,y) in rtt_maxima: #Cycle through the maxima, seperating them into two lists, x and y
+    rtt_maxima_xpos.append(x)
+    rtt_maxima_ypos.append(y)
+  for (x,y) in rtt_minima: #Cycle through the minima, seperating them into two lists, x and y
+    rtt_minima_xpos.append(x)
+    rtt_minima_ypos.append(y)
+  plt.plot(rtt_maxima_xpos, rtt_maxima_ypos, marker = '.', linestyle = '', color = 'g')
+  plt.plot(rtt_minima_xpos, rtt_minima_ypos, marker = '.', linestyle = '', color = 'b')
+  
+  #Next, do Time To Live
+  plt.subplot(2,1,2) #2 rows, 1 column, fig. 2
+  plt.title("TTL from " + host + " (" + hostaddr + "), " + str(index) + " samples.")
+  plt.xlim(0,index)
+  plt.xlabel("ICMP Sequence Number")
+  plt.ylabel("TTL Value (hops remaining)")
+  plt.grid(True)
+  plt.plot(np.array([n for n in range (index)]), np.array(ttl_list), linestyle = '-', marker = '', color = 'g')
   plt.show()
   
 #end build_graph_for_ping_file()
